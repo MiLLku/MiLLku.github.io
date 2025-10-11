4127f1881b4d17acfc788e7c18f7e2f0f7d4c8e6
MillKu <ptrol123@naver.com>
2025-10-11 23:06:38 +0900

    지도 슬라이더

---
+++ assets/js/custom.js
@@ -0,0 +1,48 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (slides.length === 0) return; 
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+setInterval(function() {
+  plusSlides(1);
+}, 3000);
+
+
+
+function initMap() {
+    if (document.getElementById('map')) {
+        var map = L.map('map').setView([35.8469, 127.1293], 15);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([35.8469, 127.1293]).addTo(map)
+            .bindPopup('전북대학교 전주캠퍼스')
+            .openPopup();
+    }
+}
+
+var leafletScript = document.createElement('script');
+leafletScript.src = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js';
+leafletScript.integrity = 'sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==';
+leafletScript.crossOrigin = '';
+leafletScript.onload = function () {
+    initMap();
+};
+document.head.appendChild(leafletScript);

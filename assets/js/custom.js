106eb16331cd7cd53e6186da199f97cd0acf9667
MillKu <ptrol123@naver.com>
2025-10-11 23:20:10 +0900

    오류

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,45 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
 
-  let slideIndex = 1;
-  const slides = document.getElementsByClassName("slide");
+  const sliderContainer = document.querySelector('.slider-container');
+  if (sliderContainer) {
+    let slideIndex = 1;
+    const slides = sliderContainer.querySelectorAll(".slide");
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
 
-  if (slides.length > 0) {
+    function showSlides(n) {
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[slideIndex - 1].style.display = "block";
+    }
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    if(prevBtn) {
+        prevBtn.addEventListener('click', () => plusSlides(-1));
+    }
+    if(nextBtn) {
+        nextBtn.addEventListener('click', () => plusSlides(1));
+    }
+
     showSlides(slideIndex);
 
-    setInterval(function() {
-      plusSlides(1);
-    }, 3000);
+    setInterval(() => plusSlides(1), 3000);
   }
 
-  window.plusSlides = function(n) {
-    showSlides(slideIndex += n);
-  }
 
-  function showSlides(n) {
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    try {
+        var map = L.map('map').setView([35.8469, 127.1293], 15);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        L.marker([35.8469, 127.1293]).addTo(map)
+          .bindPopup('전북대학교 전주캠퍼스')
+          .openPopup();
+    } catch(e) {
+
     }
-    slides[slideIndex - 1].style.display = "block";
-  }
-
-  if (document.getElementById('map')) {
-    var leafletScript = document.createElement('script');
-    leafletScript.src = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js';
-    leafletScript.integrity = 'sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==';
-    leafletScript.crossOrigin = '';
-    
-    leafletScript.onload = function () {
-      var map = L.map('map').setView([35.8469, 127.1293], 15);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-      L.marker([35.8469, 127.1293]).addTo(map)
-        .bindPopup('전북대학교 전주캠퍼스')
-        .openPopup();
-    };
-    document.head.appendChild(leafletScript);
   }
 
 });
